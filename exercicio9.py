8f5f13943b544c676efc077de31465d1e7317a28
ArieliSmiderle <arieli.rosa@monacobr.com.br>
2022-11-01 15:00:08 -0300

    conversão de metros

---
+++ exercicio9.py
@@ -0,0 +1,5 @@
+#ler um valor em metros e mostrar convertido em centímetros e milímetros
+n = int(input('Digite um número em metros:'))
+c = n * 100
+m = n * 1000
+print('A conversão do número {} em centímetros é {} e em milímetros é {}'.format(n, c, m))
