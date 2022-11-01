9726399c255ddb99b5abed604ae38c4378129141
ArieliSmiderle <arieli.rosa@monacobr.com.br>
2022-11-01 14:19:05 -0300

    exercicios

---
+++ exercício2.py
@@ -0,0 +1,3 @@
+#calcular o antecessor e o sucessor de um número
+n = int(input('Digite um número:'))
+print('Vendo o número digitado {}, podemos ver que seu antecessor é {} e seu sucessor é {}'.format(n, (n-1), (n+1)))
---
+++ exercício4.py
@@ -0,0 +1,3 @@
+#lendo um número
+n = int(input('Digite um número'))
+print('O número digitado foi {}'. format(n))
