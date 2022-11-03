b21be85de839dab3b8616bcf6dbcd81fe83b8bc9
ArieliSmiderle <arieli.rosa@monacobr.com.br>
2022-11-03 15:22:52 -0300

    decisão

---
+++ exdecisao1.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+if n1 > n2:
+    print('O número {} é maior que o número {}'.format(n1, n2))
+if n2 > n1:
+    print('O número {} é maior que o número {}'.format(n2, n1))
