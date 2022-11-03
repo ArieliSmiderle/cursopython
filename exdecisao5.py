8b7172c57078927978deca48186e6b79857167f9
ArieliSmiderle <arieli.rosa@monacobr.com.br>
2022-11-03 16:35:26 -0300

    média

---
+++ exdecisao5.py
@@ -0,0 +1,9 @@
+n1 = float(input('Digite a primeira nta do aluno: '))
+n2 = float(input('Digite a segunda nota do aluno: '))
+m = (n1 + n2) / 2
+if m == 10:
+    print('Aprovado com distinção')
+elif m >= 7:
+    print('Aprovado')
+else:
+    print('Reprovado')
