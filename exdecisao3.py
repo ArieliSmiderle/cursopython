9f3a17f81098f6603ef67201bf8cb712e4f681f8
ArieliSmiderle <arieli.rosa@monacobr.com.br>
2022-11-03 15:55:44 -0300

    escolha de sexo

---
+++ exdecisao3.py
@@ -0,0 +1,8 @@
+# -*- coding: latin-1 -*
+s = input('Digite (f)-feminino, (m)-masculino: ')
+if s == 'f':
+    print('O sexo da pessoa é feminino')
+elif s == 'm':
+    print('O sexo da pessoa é masculino')
+else:
+    print('O sexo digitado é inválido')
