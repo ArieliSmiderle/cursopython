110cd57534a72a31ffc099c624206886629c29ae
ArieliSmiderle <arieli.rosa@monacobr.com.br>
2022-11-07 21:46:01 -0300

    soma e média

---
+++ exrepeticao7.py
@@ -0,0 +1,8 @@
+n1 = float(input('Digite o primeiro número: '))
+n2 = float(input('Digite o segundo número: '))
+n3 = float(input('Digite o terceiro número: '))
+n4 = float(input('Digite o quarto número'))
+n5 = float(input('Digite o quinto número: '))
+soma = n1 + n2 + n3 + n4 + n5
+media = soma / 2
+print('A soma entre os números {}, {}, {}, {}, e {}, é {} e a sua média é {}'.format(n1, n2, n3, n4, n5, soma, media))
