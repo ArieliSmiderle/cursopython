6ba7815138aaaba597b3fd8fab9fb41f6ed64160
ArieliSmiderle <arieli.rosa@monacobr.com.br>
2022-11-07 21:14:07 -0300

    população

--- exrepeticao4.py
+++ exrepeticao4.py
@@ -1,8 +1,8 @@
-paisA = 80000
-paisB = 200000
+paisA = float(input('Digite o número da primeira população: '))
+paisB = float(input('Digite o número da segunda população: '))
 ano = 0
 while paisA < paisB:
-    paisA += paisA * 3 /100
+    paisA += paisA * 3 / 100
     paisB += paisB * 1.5 / 100
     ano += 1
-print ('O pais A ultrapassa ou iguala B em {} anos'.format(ano))
+print('O pais A ultrapassa ou iguala B em {} anos'.format(ano))
