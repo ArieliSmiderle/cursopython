329d81344bd4d61e2cb618955be7597b5a95f461
ArieliSmiderle <arieli.rosa@monacobr.com.br>
2022-11-12 15:00:02 -0300

    fatorial

--- exrepeticao14.py
+++ exrepeticao14.py
@@ -1,8 +1,10 @@
-#calcular o fatorial de um número
+#calcular o fatorial de um número e repetir
 n = int(input('Digite um número: '))
 resultado = 1
 count = 1
+if (n < 0) or (n >= 16):
+    print ('Digite um número inteiro, positivo e menor que 16: ')
 while count <= n:
     resultado *= count
     count += 1
-print (resultado)
+print(' = {}'.format(resultado))
