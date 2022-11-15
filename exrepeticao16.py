423d4e243ed07da5f175ee320117a84ea5197cd3
ArieliSmiderle <arieli.rosa@monacobr.com.br>
2022-11-14 21:29:00 -0300

    números primos

--- exrepeticao16.py
+++ exrepeticao16.py
@@ -1,9 +1,10 @@
 num = int(input('Digite um número: '))
-mult=0
+mult = 0
 for count in range(2, num):
-    if(num % count == 0):
+    if num % count == 0:
+        print('Divisível por', count)
         mult += 1
-if (mult == 0):
+if mult == 0:
     print('O número {} é um número primo'.format(num))
 else:
     print('O número {} não é um número primo'.format(num))
