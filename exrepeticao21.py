991671fa5675f72d643de1159c327dab304dc193
ArieliSmiderle <arieli.rosa@monacobr.com.br>
2022-11-16 21:25:39 -0300

    média dos cds

---
+++ exrepeticao21.py
@@ -0,0 +1,9 @@
+cds = int(input("Digite quantos cds você comprou: "))
+a = 0
+total = 0
+for i in range(cds):
+    valor = float(input('Digite qual o valor de cada cd: R$ '))
+    total = total + valor
+    media = total / cds
+    a = a + 1
+    print(f"O valor médio gasto nos cds foi R$ {media}")
