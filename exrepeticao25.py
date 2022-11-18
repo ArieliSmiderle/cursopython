f64821ea973ebed6f76c665e0323fb7b894c190b
ArieliSmiderle <arieli.rosa@monacobr.com.br>
2022-11-18 20:23:11 -0300

    fatorial de um número

--- exrepeticao25.py
+++ exrepeticao25.py
@@ -1,7 +1,7 @@
 n = int(input('Digite um número para calcular seu fatorial: '))
 c = n
 f = 1
-print('O fatorial de {}! é igual:'.format(n), end='')
+print('O fatorial de {}! é igual: '.format(n), end='')
 while c > 0:
     print('{}'.format(c), end='')
     print(' * ' if c > 1 else ' = ', end='')
