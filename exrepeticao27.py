0b11f10ca938c4a37faba3280aae5cd89fa0c1c7
ArieliSmiderle <arieli.rosa@monacobr.com.br>
2022-11-20 13:52:29 -0300

    números primos

---
+++ exrepeticao27.py
@@ -0,0 +1,12 @@
+def primo(n):
+    for val in range(2, n):
+        if n % val == 0:
+            return False
+        return True
+def exibe():
+    n = int(input('Exibir os números primos até o número: '))
+    for val in range(2, n + 1):
+        if primo(val):
+            print(val)
+while True:
+    exibe()
